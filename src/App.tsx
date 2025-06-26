a0cdf9dc9210ab2d756d1005c776e74f4322d267
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-26 14:58:34 +0530

    Updated code files via regeneration: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,13 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PublicClientApplication } from "@azure/msal-browser";
-import { MsalProvider } from "@azure/msal-react";
-import { msalConfig } from "./authConfig";
 
-// Layout and Pages
-import MainLayout from "./components/layout/MainLayout";
+// Auth
+import { AuthProvider } from "./components/auth/AuthProvider";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import SignInPage from "./pages/SignInPage";
+
+// Pages
 import ArtistPage from "./pages/ArtistPage";
 import HomePage from "./pages/HomePage";
 import LibraryPage from "./pages/LibraryPage";
@@ -16,39 +17,35 @@ import PlaylistPage from "./pages/PlaylistPage";
 import SearchPage from "./pages/SearchPage";
 import NotFound from "./pages/NotFound";
 
-/**
- * IMPORTANT: To use MSAL, you must install the necessary packages. Run:
- * 
- * npm install @azure/msal-browser @azure/msal-react
- * 
- * or the equivalent for your package manager.
- */
-
 const queryClient = new QueryClient();
-const msalInstance = new PublicClientApplication(msalConfig);
 
 const App = () => (
-  <MsalProvider instance={msalInstance}>
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route element={<MainLayout />}>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/artist" element={<ArtistPage />} />
-              <Route path="/library" element={<LibraryPage />} />
-              <Route path="/playlist" element={<PlaylistPage />} />
-              <Route path="/search" element={<SearchPage />} />
-            </Route>
-            {/* catch-all should be outside the main layout */}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </TooltipProvider>
-    </QueryClientProvider>
-  </MsalProvider>
+<QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+      <AuthProvider>
+        <Routes>
+          {/* Public Route */}
+          <Route path="/signin" element={<SignInPage />} />
+          
+          {/* Protected Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/artist" element={<ArtistPage />} />
+            <Route path="/library" element={<LibraryPage />} />
+            <Route path="/playlist" element={<PlaylistPage />} />
+            <Route path="/search" element={<SearchPage />} />
+          </Route>
+
+          {/* Catch-all */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </AuthProvider>
+    </BrowserRouter>
+    </TooltipProvider>
+</QueryClientProvider>
 );
 
 export default App;
